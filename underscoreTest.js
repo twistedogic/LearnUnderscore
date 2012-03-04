96f75af52d98d18c088541a1a781f58df15c6508
sfrage <sfrage@gmail.com>
2012-03-04 09:22:44 -0800

    First real test

--- underscoreTest.js
+++ underscoreTest.js
@@ -1,4 +1,8 @@
 describe("Collection", function() {
     it("should iterate over all elements", function() {
+        var array = [1,3,5,7,8,9,0];
+        var f = jasmine.createSpy('forEachCaller');
+        _.each(array, f);
+        expect(f.callCount).toEqual(7);
     });
 });
